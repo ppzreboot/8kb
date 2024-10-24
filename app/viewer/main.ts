a9075b3ee0000b62171c5d68de73d2ea1e5fa9d7
PPz <ppzreboot@qq.com>
2024-10-24 21:23:51 +0800

    feat: show mario

--- app/viewer/main.ts
+++ app/viewer/main.ts
@@ -1,72 +1,78 @@
 import { parse_8kb } from '@8kb/parse'
 import { render_tile, type color_map } from '@8kb/render'
 
-document.getElementById('file-input')!.onchange = async function(evt) {
-  // @ts-ignore
-  const file: File | undefined = evt.target.files[0]
-  if (file)
-    console.log(
-      parse_8kb(await file.arrayBuffer())
-    )
-}
-
 const canvas = document.querySelector('canvas')!
 const canvas_ctx = canvas.getContext('2d')!
+const file_input = document.getElementById('file-input')!
 
 const color_map: color_map = {
   0: [0,0,0,0],
-  1: [255,0,0,255],
-  2: [0,255,0,255],
-  3: [0,0,255,255],
+  1: [237,159,35,255],
+  2: [182,48,32,255],
+  3: [107,109,0,255],
 }
 
-render_tile({
-  dx: 0,
-  dy: 0,
-  color_map,
-  ctx: canvas_ctx,
-  tile: [ // 8 * 8
-    0, 0, 0, 1, 1, 0, 0, 0,
-    0, 0, 1, 0, 0, 1, 0, 0,
-    0, 0, 1, 0, 0, 1, 0, 0,
-    0, 0, 0, 1, 1, 0, 0, 0,
-    0, 0, 1, 0, 0, 1, 0, 0,
-    0, 0, 1, 0, 0, 1, 0, 0,
-    0, 0, 0, 1, 1, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-  ],
-})
+file_input.onchange = async function(evt) {
+  // @ts-ignore
+  const file: File | undefined = evt.target.files[0]
+  if (!file) return
 
-render_tile({
-  dx: 8,
-  dy: 0,
-  color_map,
-  ctx: canvas_ctx,
-  tile: [ // 8 * 8
-    0, 0, 2, 0, 0, 0, 0, 0,
-    0, 0, 2, 0, 0, 0, 0, 0,
-    0, 0, 2, 0, 0, 2, 0, 0,
-    0, 0, 2, 0, 2, 0, 0, 0,
-    0, 0, 2, 2, 0, 0, 0, 0,
-    0, 0, 2, 0, 2, 0, 0, 0,
-    0, 0, 2, 0, 0, 2, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-  ],
-})
+  const tiles = parse_8kb(await file.arrayBuffer())
 
-render_tile({
-  dx: 16,
-  dy: 0,
-  color_map,
-  ctx: canvas_ctx,
-  tile: [ // 8 * 8
-    0, 0, 3, 0, 0, 0, 0, 0,
-    0, 0, 3, 0, 0, 0, 0, 0,
-    0, 0, 3, 0, 0, 0, 0, 0,
-    0, 0, 3, 3, 3, 0, 0, 0,
-    0, 0, 3, 0, 0, 3, 0, 0,
-    0, 0, 3, 0, 0, 3, 0, 0,
-    0, 0, 3, 3, 3, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-  ],
-})
+  render_tile({
+    dx: 0,
+    dy: 0,
+    color_map,
+    ctx: canvas_ctx,
+    tile: tiles[0],
+  })
+  render_tile({
+    dx: 8,
+    dy: 0,
+    color_map,
+    ctx: canvas_ctx,
+    tile: tiles[1],
+  })
+  render_tile({
+    dx: 0,
+    dy: 8,
+    color_map,
+    ctx: canvas_ctx,
+    tile: tiles[2],
+  })
+  render_tile({
+    dx: 8,
+    dy: 8,
+    color_map,
+    ctx: canvas_ctx,
+    tile: tiles[3],
+  })
+  render_tile({
+    dx: 0,
+    dy: 16,
+    color_map,
+    ctx: canvas_ctx,
+    tile: tiles[4],
+  })
+  render_tile({
+    dx: 8,
+    dy: 16,
+    color_map,
+    ctx: canvas_ctx,
+    tile: tiles[5],
+  })
+  render_tile({
+    dx: 0,
+    dy: 24,
+    color_map,
+    ctx: canvas_ctx,
+    tile: tiles[6],
+  })
+  render_tile({
+    dx: 8,
+    dy: 24,
+    color_map,
+    ctx: canvas_ctx,
+    tile: tiles[7],
+  })
+}
