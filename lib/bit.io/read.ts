624f0a4600d2b4b286a12f2851794b1f209723e5
PPz <ppzreboot@qq.com>
2024-12-27 14:08:45 +0800

    feat(lib/bit.io): read bit by index from uint8array

--- lib/bit.io/read.ts
+++ lib/bit.io/read.ts
@@ -11,22 +11,26 @@ export
 type I_bit = 1 | 0
 
 export
+const read_bit_by_index_from_u8 = (index: number, u8: Uint8Array, bit_length = u8.length * 8) => {
+  if (index < 0 || index >= bit_length)
+    return 'out of range'
+  if (!Number.isInteger(index))
+    return 'invalid index'
+
+  const [byte_index, bit_index] = divide(index, 8)
+  const byte = u8[byte_index]
+
+  let result = byte << bit_index
+  result &= 255
+  return result >> 7 as 1 | 0
+}
+
+export
 const read_bit = (ab: ArrayBuffer) => {
   const ua = new Uint8Array(ab)
   const bit_length = ab.byteLength * 8
-  return (index: number): I_bit | 'out of range' | 'invalid index' => {
-    if (index < 0 || index >= bit_length)
-      return 'out of range'
-    if (!Number.isInteger(index))
-      return 'invalid index'
-
-    const [byte_index, bit_index] = divide(index, 8)
-    const byte = ua[byte_index]
-
-    let result = byte << bit_index
-    result &= 255
-    return result >> 7 as 1 | 0
-  }
+  return (index: number): I_bit | 'out of range' | 'invalid index' =>
+    read_bit_by_index_from_u8(index, ua, bit_length)
 }
 
 export
