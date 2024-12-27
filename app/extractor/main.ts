58cfe85c0030b75eed4c561c7f7ca935dfb0906c
PPz <ppzreboot@qq.com>
2024-12-27 16:29:08 +0800

    feat: app/extractor

--- app/extractor/main.ts
+++ app/extractor/main.ts
@@ -1,23 +1,7 @@
-// import { read_bit } from '@8kb/bit.io'
+import { download_chr } from '@8kb/prom/browser'
 
-function listen_file_change() {
-  const file_input = document.getElementById('nes-file-input') as HTMLInputElement
-  file_input.addEventListener('change', async () => {
-    const file = file_input.files![0]
-    split(await file.arrayBuffer())
-  })
-}
-
-function split(array_buffer: ArrayBuffer) {
-  const nes = new Uint8Array(array_buffer)
-  const tr = document.createElement('tr')
-  const start = 0
-  for (let i=start; i<start + 16; i++) {
-    const td = document.createElement('td')
-    td.innerHTML = nes[i] + ''
-    tr.appendChild(td)
-  }
-  document.querySelector('tbody')!.appendChild(tr)
-}
-
-listen_file_change()
+const file_input = document.getElementById('nes-file-input') as HTMLInputElement
+file_input.addEventListener('change', async () => {
+  const file = file_input.files![0]
+  download_chr(file)
+})
